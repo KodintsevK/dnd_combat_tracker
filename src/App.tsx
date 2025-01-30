1f5d8b832e5a5b2b7069c4745df2d96fba7a3d06
KodintsevK <kodintsevkirill@gmail.com>
2025-01-30 18:17:19 +0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import CTable from './components/CTable/table.tsx';
+
+
+interface Player {
+  id: number;
+  name: string;
+  initiative: number;
+  armorClass: number;
+  maxHP: number;
+  damageTaken: number;
+}
+
+const App = () => {
+  // Начальные данные для таблицы
+  const [data, setData] = useState<Player[]>([]);
+
+  const [newPlayer, setNewPlayer] = useState<Omit<Player, 'id'>>({
+    name: '',
+    initiative: 0,
+    armorClass: 0,
+    maxHP: 0,
+    damageTaken: 0,
+  });
+
+  const handleEditCell = (id: number, field: keyof Player, value: string | number) => {
+    const updatedData = data.map(player => {
+      if (player.id === id) {
+        return { ...player, [field]: value };
+      }
+      return player;
+    });
+    updatedData.sort((a, b) => b.initiative - a.initiative);
+    setData(updatedData);
+  };
+
+  const handleAddPlayer = () => {
+      const updatedData = [
+        ...data,
+        {
+          id: data.length + 1,
+          ...newPlayer,
+        },
+      ];
+
+      // Сортировка по инициативе (по возрастанию)
+      updatedData.sort((a, b) => b.initiative - a.initiative);
+      setData(updatedData);
+
+  };
+
+  const hadleDeletePlayer = (id : number) => {
+    console.log(id);
+    
+    const updatedData = data.filter(e=> e.id !== id)
+    updatedData.sort((a, b) => b.initiative - a.initiative);
+    setData(updatedData);
+  }
+
+
+  return (
+    <div>
+      <h1>Таблица игроков</h1>
+
+      <CTable data={data} onEditCell={handleEditCell} onDelete={hadleDeletePlayer}/>
+
+      {
+        <button onClick={handleAddPlayer}>+</button>
+      }
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CTable/table.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+interface Player {
+  id: number;
+  name: string;
+  initiative: number;
+  armorClass: number;
+  maxHP: number;
+  damageTaken: number;
+}
+
+interface CTableProps {
+  data: Player[];
+  onEditCell: (id: number, field: keyof Player, value: string | number) => void;
+  onDelete: (id: number) => void;
+}
+
+const CTable: React.FC<CTableProps> = ({ data, onEditCell, onDelete }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Инициатива</th>
+          <th>Класс Брони</th>
+          <th>Макс. Здоровье</th>
+          <th>Получено Урона</th>
+          <th>Опции</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(player => (
+          <tr key={player.id}>
+            <td>
+              <input
+                type="text"
+                value={player.name}
+                onChange={(e) => onEditCell(player.id, 'name', e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                value={player.initiative}
+                onChange={(e) => onEditCell(player.id, 'initiative', Number(e.target.value) || '')}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                value={player.armorClass}
+                onChange={(e) => onEditCell(player.id, 'armorClass', Number(e.target.value) || '')}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                value={player.maxHP}
+                onChange={(e) => onEditCell(player.id, 'maxHP', Number(e.target.value) || '')}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                value={player.damageTaken}
+                onChange={(e) => onEditCell(player.id, 'damageTaken', Number(e.target.value) || '')}
+              />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(player.id) }
+              >Удалить</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CTable;
